eac085c57a03631e1bc1e59e71162d5dae480d04
AboFawaz305 <84806203+AboFawaz305@users.noreply.github.com>
2025-07-27 12:05:39 +0300

    added: project 9 task 1 solution

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module contains code to manipulate squares
+"""
+
+
+class Square:
+    """
+    This class represents a square
+    """
+
+    def __init__(self, size) -> None:
+        self.__size = size
