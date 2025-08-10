b04866587615d885675aa15109fdfadd93db4e13
AboFawaz305 <84806203+AboFawaz305@users.noreply.github.com>
2025-08-10 12:06:02 +0300

    added: project 13 task 10 solution

---
+++ python-input_output/10-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Class student module
+"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        return {
+                k: v for k, v in self.__dict__.items()
+                if attrs is None or k in attrs
+                }
