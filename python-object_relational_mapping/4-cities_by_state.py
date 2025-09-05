ebf7e2fb206734c7831c2bc82208bedb0aff9ccd
AboFawaz305 <84806203+AboFawaz305@users.noreply.github.com>
2025-09-06 00:45:46 +0300

    added: project 15 task 4

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+Lists all states with a name starting with N
+from the database hbtn_0e_0_usa.
+"""
+
+import sys
+
+import MySQLdb
+
+
+def list_all():
+    """
+    Lists all states with a name starting with N
+    from the database hbtn_0e_0_usa.
+    """
+    # Get MySQL credentials and database name from arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    # name = sys.argv[4]
+    db = None
+    cursor = None
+    try:
+        # Connect to MySQL server
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            # sql password and database
+            passwd=password,
+            db=database,
+        )
+        cursor = db.cursor()
+
+        query = """SELECT C.id,C.name,S.name
+            FROM cities C INNER JOIN states S ON C.state_id=S.id
+            ORDER BY C.id ASC"""
+        cursor.execute(query)
+        # Fetch all rows
+        rows = cursor.fetchall()
+        # Print each row
+        for row in rows:
+            print(row)
+    except MySQLdb.Error as e:
+        print(f"Error connecting to databse, {e}")
+    finally:
+        if cursor:
+            # Clean up
+            cursor.close()
+        if db:
+            db.close()
+
+
+if __name__ == "__main__":
+    list_all()
