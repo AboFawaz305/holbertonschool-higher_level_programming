038141ae906a3d7b6815102e1050df8c7d12593f
AboFawaz305 <84806203+AboFawaz305@users.noreply.github.com>
2025-09-05 14:13:26 +0300

    added: project 15 task 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+Lists all states with a name starting with N
+from the database hbtn_0e_0_usa.
+"""
+
+import sys
+
+import MySQLdb
+
+
+def list_all():
+    """
+    Lists all states with a name starting with N
+    from the database hbtn_0e_0_usa.
+    """
+    # Get MySQL credentials and database name from arguments
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    db = None
+    cursor = None
+
+    try:
+        # Connect to MySQL server
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            # sql password and database
+            passwd=password,
+            db=database,
+        )
+
+        # Create a cursor to execute queries
+        cursor = db.cursor()
+
+        cursor.execute(
+            # Execute SQL query to select states starting with "N"
+            "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
+        )
+
+        # Fetch all rows
+        rows = cursor.fetchall()
+
+        # Print each row
+        for row in rows:
+            print(row)
+    except MySQLdb.Error as e:
+        print(f"Error connecting to databse, {e}")
+    finally:
+        if cursor:
+            # Clean up
+            cursor.close()
+        if db:
+            db.close()
+
+
+if __name__ == "__main__":
+    list_all()
---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+Lists all states with a name starting with N
+from the database hbtn_0e_0_usa.
+"""
+
+import sys
+
+import MySQLdb
+
+
+def list_all():
+    """
+    Lists all states with a name starting with N
+    from the database hbtn_0e_0_usa.
+    """
+    # Get MySQL credentials and database name from arguments
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    name = sys.argv[4]
+    db = None
+    cursor = None
+
+    try:
+        # Connect to MySQL server
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            # sql password and database
+            passwd=password,
+            db=database,
+        )
+
+        # Create a cursor to execute queries
+        cursor = db.cursor()
+
+        cursor.execute(
+            # Execute SQL query to select states starting with "N"
+            "SELECT * FROM states WHERE BINARY name='{0}' ORDER BY id ASC"
+            # the name of the state
+            .format(name)
+        )
+
+        # Fetch all rows
+        rows = cursor.fetchall()
+
+        # Print each row
+        for row in rows:
+            print(row)
+    except MySQLdb.Error as e:
+        print(f"Error connecting to databse, {e}")
+    finally:
+        if cursor:
+            # Clean up
+            cursor.close()
+        if db:
+            db.close()
+
+
+if __name__ == "__main__":
+    list_all()
